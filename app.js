7e8e542af0a7eca54cb70f9776116df3086b44ce
Ikpeme <74109734+Ikpeme29@users.noreply.github.com>
2021-01-10 20:27:21 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+document.querySelector('#red').onclick = function () {
+    document.querySelector('body').style.backgroundColor = 'red';
+}
+document.querySelector('#orange').onclick = function () {
+    document.querySelector('body').style.backgroundColor = 'orange';
+}
+document.querySelector('#cyan').onclick = function () {
+    document.querySelector('body').style.backgroundColor = 'cyan';
+}
+document.querySelector('#yellow').onclick = function () {
+    document.querySelector('body').style.backgroundColor = 'yellow';
+}
+document.querySelector('#pink').onclick = function () {
+    document.querySelector('body').style.backgroundColor = 'pink';
+}
+document.querySelector('#green').onclick = function () {
+    document.querySelector('body').style.backgroundColor = 'lime';
+}
+document.querySelector('#indigo').onclick = function () {
+    document.querySelector('body').style.backgroundColor = 'indigo';
+}
+document.querySelector('#brown').onclick = function () {
+    document.querySelector('body').style.backgroundColor = 'brown';
+}
+document.querySelector('#grey').onclick = function () {
+    document.querySelector('body').style.backgroundColor = 'grey';
+}
